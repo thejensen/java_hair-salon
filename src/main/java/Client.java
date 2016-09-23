8df9e504965c301876b7c01f6f19244dd331a907
Sara Jensen <sara.jensen@idealist.org>
2016-09-23 12:06:48 -0700

    add logic to override javas equals method, save objects to the database, and retrieve the id of clients assigned from the database.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,12 +2,46 @@ import org.sql2o.*;
 
 public class Client {
   private String name;
+  private String description;
+  private int id;
 
-  public Client(String name) {
+  public Client(String name, String description) {
     this.name = name;
+    this.description = description;
   }
 
   public String getName() {
     return name;
   }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  @Override
+  public boolean equals(Object otherClient) {
+    if (!(otherClient instanceof Client)) {
+      return false;
+    } else {
+      Client newClient = (Client) otherClient;
+      return this.getName().equals(newClient.getName()) && this.getDescription().equals(newClient.getDescription());
+    }
+  }
+
+  public void save(){
+    String sql = "INSERT INTO clients (name, description) VALUES (:name, :description)";
+    try(Connection con = DB.sql2o.open()){
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("description", this.description)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -9,12 +9,45 @@ public class ClientTest {
 
   @Before
   public void setUp(){
-    client = new Client("Gabrielle");
+    client = new Client("Gabrielle", "Sunflower blonde color, every 6 months");
   }
 
   @Test
   public void client_instantiatesCorrectly_true() {
-    Client testClient = new Client("Gabrielle");
-    assertEquals(true, testClient instanceof Client);
+    assertEquals(true, client instanceof Client);
   }
+
+  @Test
+  public void getName_returnsNameOfClient_String() {
+    assertEquals("Gabrielle", client.getName());
+  }
+
+  @Test
+  public void getDescription_returnsDescriptionOfClient_String() {
+    assertEquals("Sunflower blonde color, every 6 months", client.getDescription());
+  }
+
+  @Test
+  public void equals_returnsTrueIfPropertiesAreTheSame_true() {
+    Client secondClient = new Client("Gabrielle", "Sunflower blonde color, every 6 months");
+    assertTrue(client.equals(secondClient));
+  }
+
+  @Test
+  public void save_savesObjectToDatabase_true(){
+    client.save();
+    String sql = "SELECT * FROM clients WHERE name='Gabrielle'";
+    Client secondClient;
+    try(Connection con = DB.sql2o.open()){
+      secondClient = con.createQuery(sql).executeAndFetchFirst(Client.class);
+    }
+    assertTrue(client.equals(secondClient));
+  }
+
+  @Test
+  public void getId_returnsIdOfClient_true() {
+    client.save();
+    assertTrue(client.getId() > 0);
+  }
+
 }
