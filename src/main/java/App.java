93b9728a0974ee08add19f2c2092a73cd829c0f0
Sara Jensen <sara.jensen@idealist.org>
2016-09-25 13:51:23 -0700

    fix client form to input stylist id as an input value to assign the client to the stylist, adjust layout of stylist profile with rows/columns and a jumbotron

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -44,8 +44,17 @@ public class App {
       Map<String, Object> model = new HashMap<String, Object>();
       Stylist stylist  = Stylist.find(Integer.parseInt(request.params("id")));
       model.put("stylist", stylist);
-      // model.put("clients", stylist.getClients());
+      model.put("clients", stylist.getClients());
       model.put("template", "templates/stylist.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/stylist/:id/client/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist  = Stylist.find(Integer.parseInt(request.params("id")));
+      model.put("stylist", stylist);
+      model.put("clients", stylist.getClients());
+      model.put("template", "templates/client-form.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
@@ -53,11 +62,13 @@ public class App {
       Map<String, Object> model = new HashMap<String, Object>();
       String name = request.queryParams("name");
       String description = request.queryParams("description");
-      model.put("name", name);
-      model.put("description", description);
+      int stylist_id = Integer.parseInt(request.queryParams("stylistId"));
       Client client = new Client(name, description, stylist_id);
       client.save();
       Stylist stylist = Stylist.find(Integer.parseInt(request.params("id")));
+      model.put("clients", stylist.getClients());
+      model.put("name", name);
+      model.put("description", description);
       model.put("stylist", stylist);
       model.put("template", "templates/stylist.vtl");
       return new ModelAndView(model, layout);
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
